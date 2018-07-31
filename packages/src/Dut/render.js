f46578a629f6d9f07d5b940fb7d0fb3dda586c2a
Dragon <1641627793@qq.com>
2018-07-31 21:42:53 +0800

    props

--- packages/src/Dut/render.js
+++ packages/src/Dut/render.js
@@ -1,4 +1,9 @@
-export const mapProps = function(domNode, props) {
+export const mapProps = function(domNode, props, Vnode) {
+	if (Vnode && typeof Vnode.type === "function") {
+		//如果是组件，则不要map他的props进来
+		return;
+	}
+
 	for (let propsName in props) {
 		if (propsName === "children") continue; //children单独处理
 
